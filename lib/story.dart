5b66e0b32cdd49a3f86c1b818d83cd2e5dd6860e
Baeyung <ahmadsheikh617@gmail.com>
2022-01-02 11:10:59 +0500

    Initial commit

---
+++ lib/story.dart
@@ -0,0 +1,11 @@
+class Story {
+  late String storyTitle;
+  late String choice1;
+  late String choice2;
+
+  Story(String title, String c1, String c2) {
+    storyTitle = title;
+    choice1 = c1;
+    choice2 = c2;
+  }
+}
